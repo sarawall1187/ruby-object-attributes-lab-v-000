0cba13744b09be4c261ee910ae2540d710e0789e
Sara Walker <sarawall1187@gmail.com>
2018-12-03 18:13:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  def name 
+    @name 
+  end
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def breed 
+      @breed
+  end
+  def breed=(dog_breed)
+      @breed = dog_breed
+    end
+  end
+
